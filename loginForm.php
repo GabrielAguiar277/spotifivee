2c3663010532e55f3776d4452edf559c53bf3b69
GabrielAguiar277 <gabriel.aguiar.ti@outlook.com>
2021-11-16 22:32:51 -0200

    form login

--- loginForm.php
+++ loginForm.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../view/css/formStyle.css">
+    <link rel="stylesheet" href="./view/css/formStyle.css">
     <title>Login - Spotifive</title>
 </head>
 <body>
@@ -25,6 +25,6 @@
         </form>
     </div>
 
-    <script src="../js/label.js"></script>
+    <script src="./js/label.js"></script>
 </body>
 </html>
--- model/modelUsuario.php
+++ model/modelUsuario.php
@@ -16,4 +16,29 @@ function listaTudoUsuario($conexao){
     $resultado = mysqli_query($conexao,$query);
     return $resultado;
 }
+
+function listaUsuarioCodigo($conexao, $codigoUsu){
+    $query = "select * from tb_usuario where Id_usua = "
+}
+
+function listaUsuarioEmail($conexao, $emailUsu){
+    $query = "select * from tb_usuario where Email_Usua like '%{$emailUsu}%'";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+}
+
+function deletarUsuario($conexao, $codUsu){
+    $query = "delete from tb_usuario where Id_Usua = '{$codUsu}'";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+}
+
+function alterarUsuario($conexao, $email, $senha, $codUsu){
+    $opcao = ['cost' => 8];
+    $senhaCrypto=password_hash($senha, PASSWORD_BCRYPT,$opcao);
+
+    $query = "update tb_usuario set EmailUsua = '{$email}', Senha_Usua='{$senhaCrypto}' where Id_Usua = '{$codUsu}'";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+}
     ?>
